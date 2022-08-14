f347b1d082d0672ae879706f0e500efee248b68f
ibrohim021 <dim_aviasky@mail.ru>
2022-08-14 18:01:07 +0300

    exe-1

---
+++ exe-1/Program.cs
@@ -0,0 +1,19 @@
+﻿// //Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+var numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+
+int count = 1;
+
+while(count <= number )
+{
+    var qube = Math.Pow(count, 3);
+    Console.WriteLine(qube);
+    count++;
+
+}
+
